4d2f9ef37fedf19c2cbd488f5e0d9da70ea03f57
ynpan <ynpan@thoughtworks.com>
2020-03-07 16:29:15 +0800

    add selectionSort

---
+++ src/选择排序.js
@@ -0,0 +1,17 @@
+function selectionSort(a) {
+    for (let i = 0; i < a.length; i++) {
+        let max = 0;
+        for (let j = 0; j < a.length - i; j++) {
+            if (a[max] < a[j]) {
+                max = j;
+            }
+        }
+        let temp = a[a.length - i - 1];
+        a[a.length - i - 1] = a[max];
+        a[max] = temp;
+    }
+}
+
+
+const arr = [4, 2, 4, 1, 5, 6];
+selectionSort(arr);
